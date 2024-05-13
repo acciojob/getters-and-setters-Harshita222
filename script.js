fcc3263704dccddac86f632436ea4cedbe6b8127
Harshita222 <124767756+Harshita222@users.noreply.github.com>
2024-05-13 19:13:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
 //complete this code
 class Person {
 	constructor(name,age){
-		this.name= name;
-		this.age = age;
+		this._name= name;
+		this._age = age;
 	}
 	get name(){
-		return this.name;
-	};
-	set age(){
-		return this.age;
+		return this._name;
+	}
+	set age(newAge){
+		return this._age = newAge;
 	};
 }
 
 class Student extends Person {
+	constructor(name, age){
 	super(name,age);
+	}
 	study(){
-		console.log(`${this.name} is studying`)
+		console.log(`${this._name} is studying`)
 	}
 }
 
 class Teacher extends Person {
+	constructor(name, age){
 		super(name,age);
+	}
 	teach(){
-		console.log(`${this.name} is teaching`)
+		console.log(`${this._name} is teaching`)
 	}
 }
 
